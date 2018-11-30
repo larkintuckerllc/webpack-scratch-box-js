5ec9dae98fe04836be14c3efb1b239c9df1ba8c5
John Tucker <jtucker@appirio.com>
2018-11-30 16:29:57 -0500

    Tree Shaking

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'whatwg-fetch';
 import App from './components/App';
+import { cube } from './math';
 
 const mountNode = document.getElementById('app');
 render(<App />, mountNode);
+window.console.log(cube(2));
---
+++ src/math.js
@@ -0,0 +1,8 @@
+export function square(x) {
+  window.console.log('DEAD CODE');
+  return x * x;
+}
+
+export function cube(x) {
+  return x * x * x;
+}
