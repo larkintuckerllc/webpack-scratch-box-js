0055eee2bcb359877be6231bcd38e97c4ee26fe6
John Tucker <jtucker@appirio.com>
2018-11-30 15:46:10 -0500

    Remove things that break tree shaking

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import '@babel/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { hot } from 'react-hot-loader';
 import 'whatwg-fetch';
 import App from './components/App';
 
-const AppWithHot = hot(module)(App);
 const mountNode = document.getElementById('app');
-render(<AppWithHot />, mountNode);
+render(<App />, mountNode);
